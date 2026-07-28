355ef3e4ca0d427e72f7d744e2bf2c374d9679d8
hoptoware <97616324+hoptoware@users.noreply.github.com>
2026-07-28 16:03:00 -0300

    gets stats of the latest season
    .

--- NBA/api_test1.py
+++ NBA/api_test1.py
@@ -4,15 +4,37 @@ import pandas as pd
 from nba_api.stats.static import players, teams
 from nba_api.stats.endpoints import playercareerstats, playercompare
 
-filepath = r"python\NBA\stats\AE_Stats.json"
+filepath1 = r"python\NBA\stats\AE_Stats.json"
+filepath2 = r"python\NBA\stats\NJ_Stats.json"
 
 id1 = players.find_players_by_full_name('Anthony Edwards')[0]['id']
 id2 = players.find_players_by_full_name('Nikola Jokic')[0]['id']
 
-careerStats1 = playercareerstats.PlayerCareerStats(id1).get_data_frames()[0]
-careerStats2 = playercareerstats.PlayerCareerStats(id2).get_data_frames()
+seasonStats1 = playercareerstats.PlayerCareerStats(id1).get_data_frames()[0]
+seasonStats2 = playercareerstats.PlayerCareerStats(id2).get_data_frames()[0]
 
-print(careerStats1)
 
-'''with open (filepath, 'w') as stats1:
-    stats1.write(careerStats1)'''
+'''with open (filepath1, 'w') as stats1:
+    stats1.write(seasonStats1.to_json(orient='records', lines=True))
+
+with open (filepath2, 'w') as stats2:
+    stats2.write(seasonStats2.to_json(orient='records', lines=True))'''
+
+#check for season 2025-26
+for i in seasonStats1.index:
+    if seasonStats1.loc[i, 'SEASON_ID'] == '2025-26':
+        print(seasonStats1.loc[i])
+    else:
+        pass
+
+print("\n")
+
+for i in seasonStats2.index:
+    if seasonStats2.loc[i, 'SEASON_ID'] == '2025-26':
+        print(seasonStats2.loc[i])
+    else:
+        pass
+    
+
+#print(seasonStats1.loc[0, 'SEASON_ID'])
+#print(seasonStats2)
