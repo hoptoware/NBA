61e3889c95a4cdf648d14163b08dd3ef1bfd5576
Hugo <97616324+hoptoware@users.noreply.github.com>
2026-08-11 16:56:56 -0300

    Add files via upload

--- NBA/tests/api_test4.py
+++ NBA/tests/api_test4.py
@@ -8,10 +8,17 @@ try:
     with open (filepath, 'r') as stats:
         pass #check for file
 
-    filepath = fr"NBA\stats\{initials}_Stats1.json"
-    with open (filepath, 'w') as stats:
-        stats.write() #if exists, write with another name
-
+    for i in range(1, 9999):
+        try:
+            filepath = fr"NBA\stats\{initials}_Stats{i}.json"
+            with open (filepath, 'r') as stats:
+                pass #check for file
+        except:
+            with open (filepath, 'w') as stats:
+                stats.write() #if exists, write with another name
+            break #if file doesn't exist, break the loop and save it with that name    
+        
 except: #if doesn't exist
     with open (filepath, 'w') as stats:
         stats.write() #save it with default name
+
---
+++ NBA/tests/api_test5.py
@@ -0,0 +1,6 @@
+#line 87
+#TODO: centralize the text
+
+whitespace = " "
+
+print(f"{int((20 - len("POINTS"))/2)*whitespace}")
