d1206b06eaa223051c68ecfd0b8e21df3912c7c2
Hugo <97616324+hoptoware@users.noreply.github.com>
2026-08-11 16:56:13 -0300

    improve file handling and text centralization

--- NBA/NBA.py
+++ NBA/NBA.py
@@ -22,15 +22,21 @@ id1 = player1['id']
 filepath1 = fr"NBA\stats\{player1['initials']}_Stats.json" #these filepaths might not work on other computers
 seasonStats1 = playercareerstats.PlayerCareerStats(id1).get_data_frames()[0] #get the first dataframe of the list (which contains all career stats)
 
-try: #save stats as json files
-    with open (filepath1, 'r') as stats1:
+try:
+    with open (filepath1, 'r') as stats:
         pass #check for file
-    filepath1 = fr"NBA\stats\{player1['initials']}_Stats1.json"
-    with open (filepath1, 'w') as stats1:
-        stats1.write(seasonStats1.to_json(orient='records', lines=True)) #if exists, write with another name
+    for i in range(1, 9999):
+        try:
+            filepath1 = fr"NBA\stats\{player1['initials']}_Stats{i}.json"
+            with open (filepath1, 'r') as stats:
+                pass #check for file
+        except:
+            with open (filepath1, 'w') as stats:
+                stats.write(seasonStats1.to_json(orient='records', lines=True)) #if exists, write with another name
+            break #if file doesn't exist, break the loop and save it with that name      
 except: #if doesn't exist
-    with open (filepath1, 'w') as stats1:
-        stats1.write(seasonStats1.to_json(orient='records', lines=True)) #save it with default name
+    with open (filepath1, 'w') as stats:
+        stats.write(seasonStats1.to_json(orient='records', lines=True)) #save it with default name
 
 #check for season 2025-26 and print the stats for that season
 for i in seasonStats1.index:
@@ -51,14 +57,20 @@ filepath2 = fr"NBA\stats\{player2['initials']}_Stats.json"
 seasonStats2 = playercareerstats.PlayerCareerStats(id2).get_data_frames()[0]
 
 try:
-    with open (filepath2, 'r') as stats2:
-        pass 
-    filepath2 = fr"NBA\stats\{player2['initials']}_Stats1.json"
-    with open (filepath2, 'w') as stats2:
-        stats2.write(seasonStats2.to_json(orient='records', lines=True))
+    with open (filepath2, 'r') as stats:
+        pass #check for file
+    for i in range(1, 9999):
+        try:
+            filepath2 = fr"NBA\stats\{player2['initials']}_Stats{i}.json"
+            with open (filepath2, 'r') as stats:
+                pass #check for file
+        except:
+            with open (filepath2, 'w') as stats:
+                stats.write(seasonStats2.to_json(orient='records', lines=True)) #if exists, write with another name
+            break #if file doesn't exist, break the loop and save it with that name      
 except: #if doesn't exist
-    with open (filepath2, 'w') as stats2:
-        stats2.write(seasonStats2.to_json(orient='records', lines=True)) 
+    with open (filepath2, 'w') as stats:
+        stats.write(seasonStats2.to_json(orient='records', lines=True)) #save it with default name
 
 for i in seasonStats2.index:
     if seasonStats2.loc[i, 'SEASON_ID'] == '2025-26':
@@ -83,13 +95,12 @@ else:
     print("2025-26 STATS")
     print(f"{player1['name']} vs. {player2['name']}: (Arrow points the category winner)")
 
-    #TODO: centralize the text
-    print(f"{(10 - len("POINTS"))*whitespace} POINTS {(10 - len("POINTS"))*whitespace} \n {player1['initials']}: {P1.ptsAvg} {comparison[0]['pts']} {P2.ptsAvg} :{player2['initials']}")
-    print(f"{(10 - len("REBOUNDS"))*whitespace} REBOUNDS {(10 - len("REBOUNDS"))*whitespace} \n {player1['initials']}: {P1.rebAvg} {comparison[0]['reb']} {P2.rebAvg} :{player2['initials']}")
-    print(f"{(10 - len("ASSISTS"))*whitespace} ASSISTS {(10 - len("ASSISTS"))*whitespace} \n {player1['initials']}: {P1.astAvg} {comparison[0]['ast']} {P2.astAvg} :{player2['initials']}")
-    print(f"{(10 - len("BLOCKS"))*whitespace} BLOCKS {(10 - len("BLOCKS"))*whitespace}\n {player1['initials']}: {P1.blkAvg} {comparison[0]['blk']} {P2.blkAvg} :{player2['initials']}")
-    print(f"{(10 - len("STEALS"))*whitespace} STEALS {(10 - len("STEALS"))*whitespace}\n {player1['initials']}: {P1.stlAvg} {comparison[0]['stl']} {P2.stlAvg} :{player2['initials']}")
-    print(f"{(10 - len("TURNOVERS"))*whitespace} TURNOVERS {(10 - len("TURNOVERS"))*whitespace}\n {player1['initials']}: {P1.tovAvg} {comparison[0]['tov']} {P2.tovAvg} :{player2['initials']}")
+    print(f"{int((20 - len("POINTS"))/2)*whitespace} POINTS {int((20 - len("POINTS"))/2)*whitespace}" + f"\n {player1['initials']}: {P1.ptsAvg} {comparison[0]['pts']} {P2.ptsAvg} :{player2['initials']}")
+    print(f"{int((18 - len("REBOUNDS"))/2)*whitespace} REBOUNDS {int((18 - len("REBOUNDS"))/2)*whitespace}" + f"\n {player1['initials']}: {P1.rebAvg} {comparison[0]['reb']} {P2.rebAvg} :{player2['initials']}")
+    print(f"{int((18 - len("ASSISTS"))/2)*whitespace} ASSISTS {int((18)/2)*whitespace}" + f"\n {player1['initials']}: {P1.astAvg} {comparison[0]['ast']} {P2.astAvg} :{player2['initials']}")
+    print(f"{int((18 - len("BLOCKS"))/2)*whitespace} BLOCKS {int((18 - len("BLOCKS"))/2)*whitespace}" + f"\n {player1['initials']}: {P1.blkAvg} {comparison[0]['blk']} {P2.blkAvg} :{player2['initials']}")
+    print(f"{int((18 - len("STEALS"))/2)*whitespace} STEALS {int((18 - len("STEALS"))/2)*whitespace}" + f"\n {player1['initials']}: {P1.stlAvg} {comparison[0]['stl']} {P2.stlAvg} :{player2['initials']}")
+    print(f"{int((18 - len("TURNOVERS"))/2)*whitespace} TURNOVERS {int((18 - len("TURNOVERS"))/2)*whitespace}" + f"\n {player1['initials']}: {P1.tovAvg} {comparison[0]['tov']} {P2.tovAvg} :{player2['initials']}")
 
     if comparison[1] > comparison[2]:
         print(f"\nOVERALL WINNER: {P1.name}")
