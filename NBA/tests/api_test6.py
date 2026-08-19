5f7410b59f8aa6e12bd554d4768771f2cc998471
Hugo <97616324+hoptoware@users.noreply.github.com>
2026-08-19 09:58:33 -0300

    error handling
    This update adds error handling for database operations, allowing for the creation of a new table if it doesn't exist and updating existing records. The update query now includes additional statistics such as blocks, steals, and turnovers per game.

--- NBA/tests/api_test6.py
+++ NBA/tests/api_test6.py
@@ -13,30 +13,84 @@ player_name = player_name.replace(' ', '_')
 initials = 'CA'
 filepath = fr"NBA\stats\playersStats.db"
 
-with sqlite3.connect(filepath) as connection:
-    cursor = connection.cursor()
 
-    cursor.execute(fr'''
-    CREATE TABLE IF NOT EXISTS {player_name} 
-    (season REAL,
-    ppg REAL,
-    rpg REAL,
-    apg REAL
-    );
-    ''')
+try:
+    with open (filepath, 'r') as stats:
+        pass #make sure database exists
+except:
+    with open (filepath, 'w') as stats:
+        pass
 
-    insert_query = fr'''
-    INSERT INTO {player_name} (season, ppg, rpg, apg) 
-    VALUES (?, ?, ?, ?);
-    '''
 
-    for i in seasonStats1.index:
-        values = (seasonStats1.iloc[i,1], round(float(seasonStats1.loc[i, 'PTS'] / seasonStats1.loc[i, 'GP']), 1), 
-                  round(float(seasonStats1.loc[i, 'REB'] / seasonStats1.loc[i, 'GP']), 1), 
-                  round(float(seasonStats1.loc[i, 'AST'] / seasonStats1.loc[i, 'GP']), 1))
-        cursor.execute(insert_query, values)
+try:
+    with sqlite3.connect(filepath) as connection:
+        cursor = connection.cursor()
 
-    connection.commit()
+        selection_query = fr'''
+        SELECT * FROM {player_name};
+        '''
 
-print(seasonStats1)
-print(seasonStats1.loc[i, 'PTS'])
+        cursor.execute(selection_query)
+
+        all_students = cursor.fetchall()
+
+        
+        update_query = fr'''
+            UPDATE {player_name} 
+            SET ppg = ?,
+                rpg = ?,
+                apg = ?,
+                bpg = ?,
+                spg = ?,
+                tovpg = ?
+            WHERE season = ?;
+            '''
+
+        for i in seasonStats1.index:
+            values = (
+                    round(float(seasonStats1.loc[i, 'PTS'] / seasonStats1.loc[i, 'GP']), 1), 
+                    round(float(seasonStats1.loc[i, 'REB'] / seasonStats1.loc[i, 'GP']), 1), 
+                    round(float(seasonStats1.loc[i, 'AST'] / seasonStats1.loc[i, 'GP']), 1),
+                    round(float(seasonStats1.loc[i, 'BLK'] / seasonStats1.loc[i, 'GP']), 1),
+                    round(float(seasonStats1.loc[i, 'STL'] / seasonStats1.loc[i, 'GP']), 1),  
+                    round(float(seasonStats1.loc[i, 'TOV'] / seasonStats1.loc[i, 'GP']), 1),
+                    seasonStats1.loc[i, 'SEASON_ID'])
+            cursor.execute(update_query, values)
+
+
+        print("table updated")
+except:
+        with sqlite3.connect(filepath) as connection:
+            cursor = connection.cursor()
+
+            cursor.execute(fr'''
+            CREATE TABLE IF NOT EXISTS {player_name} 
+            (season REAL,
+            ppg REAL,
+            rpg REAL,
+            apg REAL,
+            bpg REAL,
+            spg REAL,
+            tovpg REAL
+            );
+            ''')
+
+            insert_query = fr'''
+            INSERT INTO {player_name} (season, ppg, rpg, apg, bpg, spg, tovpg) 
+            VALUES (?, ?, ?, ?, ?, ?, ?);
+            '''
+
+            for i in seasonStats1.index:
+                values = (seasonStats1.loc[i, 'SEASON_ID'], 
+                        round(float(seasonStats1.loc[i, 'PTS'] / seasonStats1.loc[i, 'GP']), 1), 
+                        round(float(seasonStats1.loc[i, 'REB'] / seasonStats1.loc[i, 'GP']), 1), 
+                        round(float(seasonStats1.loc[i, 'AST'] / seasonStats1.loc[i, 'GP']), 1),
+                        round(float(seasonStats1.loc[i, 'BLK'] / seasonStats1.loc[i, 'GP']), 1),
+                        round(float(seasonStats1.loc[i, 'STL'] / seasonStats1.loc[i, 'GP']), 1),  
+                        round(float(seasonStats1.loc[i, 'TOV'] / seasonStats1.loc[i, 'GP']), 1))
+                cursor.execute(insert_query, values)
+
+            connection.commit()
+
+
+            print("table created or not found")
